6576978501884132c79abb581fe5507e913a294d
wooseokoh <48680896+wooseokoh@users.noreply.github.com>
2019-04-30 21:21:57 +0900

    특수문자

---
+++ java02/src/java02/MainClass.java
@@ -0,0 +1,26 @@
+package java02;
+
+public class MainClass {
+	public static void main(String[] args) {
+		
+		char c = 'a';
+		System.out.println("c = " + c);
+		
+		int i = 10;
+		System.out.println("i = " + i);
+		
+		double d = 10.123;
+		System.out.println("d = " + d);
+		
+		boolean b = false;
+		System.out.println("b = " + b);
+		
+		String s = "Hello World";
+		System.out.println("s = " + s);
+		System.out.println(s);
+		
+		s = "Good";
+		System.out.println("s = " + s);
+		
+	}
+}
---
+++ java02/src/java02/SecondClass.java
@@ -0,0 +1,18 @@
+package java02;
+
+public class SecondClass {
+	public static void main(String[] args) {
+		
+		System.out.println("GOOD\tMORNING");
+		System.out.println("GOOD\nMORNING");
+		System.out.println("GOOD\'MORNING");
+		System.out.println("GOOD\"MORNING");
+		System.out.println("GOOD\\MORNING");
+		System.out.println("GOOD\n\nMORNING");
+		System.out.println("\'GOODMORNING~\'");
+		System.out.println("\"GOOD\tMORNING\"");
+
+		
+	}
+
+}
